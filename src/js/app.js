5577f1ab97d41f88cc72a0ed9cb6e266d1046548
Sukh Kalsi <SukhKalsi@users.noreply.github.com>
2016-09-12 23:50:15 +0100

    Small clean up

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 require('angular');
-var oddsController = require('./controllers/oddsController');;
+var oddsController = require('./controllers/oddsController');
 var dataService = require('./services/dataService');
 var formatBetFilter = require('./filters/formatBet');
 
